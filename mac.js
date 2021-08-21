5c85de8ed7eedf433d6cc641580b932aa81d1080
Hilful <hilful25@gmail.com>
2021-08-21 23:31:01 +0600

    final commit

---
+++ mac.js
@@ -0,0 +1,73 @@
+document.getElementById('cost-memory0').addEventListener('click',  function(){
+    const oldPrice = document.getElementById('Extra-Memory');
+    const updatePrice = oldPrice.innerText;
+    oldPrice.innerText = 0; 
+    updateTotal()
+})
+document.getElementById('cost-memory16').addEventListener('click',  function(){
+    const oldPrice = document.getElementById('Extra-Memory');
+    const updatePrice = oldPrice.innerText;
+    oldPrice.innerText = 180; 
+    updateTotal()
+})
+// js code for storage
+document.getElementById('cost-storage256').addEventListener('click',  function(){
+    const oldPrice = document.getElementById('Extra-Storage');
+    const updatePrice = oldPrice.innerText;
+    oldPrice.innerText = 0;
+    updateTotal() 
+})
+document.getElementById('cost-storage512').addEventListener('click',  function(){
+    const oldPrice = document.getElementById('Extra-Storage');
+    const updatePrice = oldPrice.innerText;
+    oldPrice.innerText = 100; 
+    updateTotal()
+})
+document.getElementById('cost-storage1TB').addEventListener('click',  function(){
+    const oldPrice = document.getElementById('Extra-Storage');
+    const updatePrice = oldPrice.innerText;
+    oldPrice.innerText = 180; 
+    updateTotal()
+})
+
+// js code for  delivery charge
+document.getElementById('free-delivery').addEventListener('click',  function(){
+    const oldPrice = document.getElementById('Delivery-Charge');
+    const updatePrice = oldPrice.innerText;
+    oldPrice.innerText = 0; 
+    updateTotal()
+})
+document.getElementById('charge-delivery').addEventListener('click',  function(){
+    const oldPrice = document.getElementById('Delivery-Charge');
+    const updatePrice = oldPrice.innerText;
+    oldPrice.innerText = 20;
+    updateTotal() 
+})
+
+function updateTotal(){
+    
+   
+    const bestPrice=parseInt(document.getElementById('Best-Price').innerText);
+    const memoryPrice =parseInt(document.getElementById('Extra-Memory').innerText);
+    const storagePrice =parseInt(document.getElementById('Extra-Storage').innerText);
+    const deliveryPrice =parseInt(document.getElementById('Delivery-Charge').innerText);
+    const totalPrice=document.getElementById('total-price');
+    
+    totalPrice.innerText=bestPrice+memoryPrice+storagePrice+deliveryPrice;
+
+    // updated price after using code 
+
+    const offer=document.getElementById('total-with-code');
+    offer.innerText=bestPrice+memoryPrice+storagePrice+deliveryPrice;
+}
+// coupon js code  
+
+document.getElementById('code-btn').addEventListener('click',function(){
+    const codeValue=document.getElementById('code-value')
+    const offer=document.getElementById('total-with-code');
+    if(codeValue.value=='stevekaku'){
+       offer.innerText=parseFloat(offer.innerText)-parseFloat(offer.innerText)*.2
+    }
+    
+    codeValue.value=''
+});
